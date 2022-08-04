b2f87b6e8432ce77e71426779f75ed5bc34bae7d
Popova Ekaterina <popova_katerina97@mail.ru>
2022-08-05 00:00:54 +0500

    005 example if else

---
+++ 005_Example_IF_ELSE/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Print user name ");
+string username = Console.ReadLine();
+
+if(username.ToLower()=="kit")
+{
+    Console.WriteLine("Welcome back, Kit!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
